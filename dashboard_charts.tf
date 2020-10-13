d7857def18db2a1b1e8dc7067713f3900f63113c
vsaggu <vsaggu@gmail.com>
2020-10-13 13:08:16 +0000

    initial commit

--- dashboard_charts.tf
+++ dashboard_charts.tf
@@ -1,8 +1,8 @@
-# data "template_file" "dashboard_json" {
-#   template = "${file(var.dashboard_json_filename)}"
-# }
+data "template_file" "dashboard_json" {
+  template = "${file(var.dashboard_json_filename)}"
+}
 
-# resource "google_monitoring_dashboard" "dashboard" {
-#   project        = var.project_name
-#   dashboard_json = "${data.template_file.dashboard_json.rendered}"
-# }
+resource "google_monitoring_dashboard" "dashboard" {
+  project        = "${var.project_name}"
+  dashboard_json = "${data.template_file.dashboard_json.rendered}"
+}

fbe2290ba261fe74a3030ad3fe24a5562df70f11
vsaggu <vsaggu@gmail.com>
2020-10-13 12:47:59 +0000

    initial commit

--- variables.tf
+++ variables.tf
@@ -1,66 +1,66 @@
-# variable "random_suffix" {
-#   description = "Enables project random id generation. Mutually exclusive with project_id being non-empty."
-#   type        = bool
-#   default     = false
-# }
+variable "random_suffix" {
+  description = "Enables project random id generation. Mutually exclusive with project_id being non-empty."
+  type        = "bool"
+  default     = false
+}
 
-# variable "org_id" {
-#   description = "The organization ID."
-#   type        = string
-#   default     = false
-# }
+variable "org_id" {
+  description = "The organization ID."
+  type        = "string"
+  default     = false
+}
 
 variable "project_name" {
   description = "The name for the project"
-  type        = string
+  type        = "string"
   default     = true
 }
 
 variable "billing_account" {
   description = "The ID of the billing account to associate this project with"
-  type        = string
+  type        = "string"
   default     = false
 }
 
 variable "environment" {
   description = "The developmnet environment that the project is in."
-  type        = string
+  type        = "string"
   default     = false
 }
 
 variable "troux_id" {
   description = "the troux id associated with the resource"
-  type        = string
+  type        = "string"
   default     = false
 }
 
 variable "cost_centre" {
   description = "The cost centre associated with the resource."
-  type        = string
+  type        = "string"
   default     = false
 }
 
-variable "activate_apis" {
-  description = "The list of apis to activate within the project."
-  type        = list(string)
-  default     = ["compute.googleapis.com", "stackdriver.googleapis.com", "logging.googleapis.com", "monitoring.googleapis.com"]
-}
+# variable "activate_apis" {
+#   description = "The list of apis to activate within the project."
+#   type        = list("string")
+#   default     = ["compute.googleapis.com", "stackdriver.googleapis.com", "logging.googleapis.com", "monitoring.googleapis.com"]
+# }
 
 variable "region" {
   description = "region of the resource"
-  type        = string
+  type        = "string"
   default     = false
 }
 
 variable "zone" {
   description = "zone of the resource"
-  type        = string
+  type        = "string"
   default     = false
 }
 
 variable "alert_name" {
   description = "Alerty Policy display name. Mandatory"
-  type        = string
+  type        = "string"
   default     = ""
 }
 
@@ -90,31 +90,31 @@ variable "threshold_value" {
 }
 
 variable "notification_id" {
-  type    = list
+  type    = "list"
   default = []
 }
 
 variable "logmetric_name" {
   description = "Logging Metric Name"
-  type        = string
+  type        = "string"
   default     = ""
 }
 
 variable "logmetric_filter" {
   description = "Filter for Logging Metric"
-  type        = string
+  type        = "string"
   default     = ""
 }
 
 variable "logmetric_metrickind" {
   description = "Metric Descriptor - metric_kind"
-  type        = string
+  type        = "string"
   default     = "DELTA"
 }
 
 variable "logmetric_valuetype" {
   description = "Metric Descriptor - value_type"
-  type        = string
+  type        = "string"
   default     = "INT64"
 }
 
